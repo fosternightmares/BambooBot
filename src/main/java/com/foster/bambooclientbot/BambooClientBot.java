c91340eef8337899b9369af2793821e9fe016fad
fosternightmares <lolzfoster@gmail.com>
2026-06-03 16:31:37 -0600

    Initial BambooClientBot Fabric bootstrap

---
+++ src/main/java/com/foster/bambooclientbot/BambooClientBot.java
@@ -0,0 +1,15 @@
+package com.foster.bambooclientbot;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BambooClientBot implements ModInitializer {
+    public static final String MOD_ID = "bambooclientbot";
+    private static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    @Override
+    public void onInitialize() {
+        LOGGER.info("[BambooClientBot] Loaded");
+    }
+}
